483cdf321f71924751bb8909b8819737fdf199e1
Gilli Daniele <daniele.gilli@studenti.unipr.it>
2012-09-13 10:02:48 +0200

    Funziona app engine + eclipse + twitter4j

--- Project/src/project/ProjectServlet.java
+++ Project/src/project/ProjectServlet.java
@@ -1,13 +1,43 @@
 package project;
 
 import java.io.IOException;
-import javax.servlet.http.*;
 
-@SuppressWarnings("serial")
+
+import javax.servlet.http.*;
+import java.util.List;
+
+import twitter4j.*;
+
+
+//import java.lang.Exception;
+
+@SuppressWarnings({ "serial"})
 public class ProjectServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
-	}
+		
+		 // The factory instance is re-useable and thread safe.
+		// The factory instance is re-useable and thread safe.
+	    Twitter twitter = new TwitterFactory().getInstance();
+	    List<Status> statuses;
+		try {
+			statuses = twitter.getHomeTimeline();
+			  System.out.println("Showing friends timeline.");
+			    for (Status status : statuses) {
+			        System.out.println(status.getUser().getName() + ":" +
+			                           status.getText());
+			    }
+		} catch (TwitterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	  
+		
+		
+		
+		response.setContentType("text/plain");
+		response.getWriter().println("Hello, world");
+	
+}
 }
---
+++ Project/war/project.jsp
@@ -0,0 +1,34 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="java.util.List" %>
+<%@ page import="com.google.appengine.api.users.User" %>
+<%@ page import="com.google.appengine.api.users.UserService" %>
+<%@ page import="com.google.appengine.api.users.UserServiceFactory" %>
+<%@ page import="com.google.appengine.api.datastore.DatastoreServiceFactory" %>
+<%@ page import="com.google.appengine.api.datastore.DatastoreService" %>
+<%@ page import="com.google.appengine.api.datastore.Query" %>
+<%@ page import="com.google.appengine.api.datastore.Entity" %>
+<%@ page import="com.google.appengine.api.datastore.FetchOptions" %>
+<%@ page import="com.google.appengine.api.datastore.Key" %>
+<%@ page import="com.google.appengine.api.datastore.KeyFactory" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+
+
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title>Progetto</title>
+  </head>
+
+  <body>
+    <h1>PROGETTO</h1>
+	
+    <table>
+      <tr>
+        <td colspan="2" style="font-weight:bold;">Available Servlets:</td>        
+      </tr>
+      <tr>
+        <td><a href="project">Project</a></td>
+      </tr>
+    </table>
+  </body>
+</html>
