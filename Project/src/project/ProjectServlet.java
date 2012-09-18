843ef06e85568bd9557f330ff20b48c92932458f
Gilli Daniele <daniele.gilli@studenti.unipr.it>
2012-09-18 11:35:12 +0200

    inserimento dati

--- Project/src/project/ProjectServlet.java
+++ Project/src/project/ProjectServlet.java
@@ -11,33 +11,95 @@ import twitter4j.*;
 
 //import java.lang.Exception;
 
-@SuppressWarnings({ "serial"})
+@SuppressWarnings({ "serial" , "unused"})
 public class ProjectServlet extends HttpServlet {
 	
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws IOException {
+	
+		// Prende parametri inseriti
+		String [] hashtags = request.getParameter("hashtag").split(",");
+		int giorno_from, mese_from, anno_from, ore_from, minuti_from;
+		int giorno_to, mese_to, anno_to, ore_to, minuti_to;
 		
-		 // The factory instance is re-useable and thread safe.
-		// The factory instance is re-useable and thread safe.
-	    Twitter twitter = new TwitterFactory().getInstance();
-	    List<Status> statuses;
+		
+		
+		
+		for(String hash:hashtags) // per ogni hashtag
+		{
+		    if (hash.startsWith("#"))  // se inizia con '#' OK
+		    {
+		    	try {
+		    		// Controllo sul data from
+		    		giorno_from = Integer.parseInt(request.getParameter("giorno_from"));
+		    		mese_from = Integer.parseInt(request.getParameter("mese_from"));
+		    		anno_from = Integer.parseInt(request.getParameter("anno_from"));
+		    		ore_from = Integer.parseInt(request.getParameter("ore_from"));
+		    		minuti_from = Integer.parseInt(request.getParameter("minuti_from"));
+		    		if (giorno_from < 1 || giorno_from > 31 || mese_from < 1 || mese_from > 12 || anno_from < 1 || ore_from < 0 || ore_from > 23 || minuti_from < 0 || minuti_from > 59 )
+		    		{
+		    			// Error nell'inserimento data from
+		    			response.sendRedirect("/project.jsp?Error=Date from not correct!");
+		    		}
+		    	
+		    		// Controllo sul data until
+		    		giorno_to = Integer.parseInt(request.getParameter("giorno_to"));
+		    		mese_to = Integer.parseInt(request.getParameter("mese_to"));
+		    		anno_to = Integer.parseInt(request.getParameter("anno_to"));
+		    		ore_to = Integer.parseInt(request.getParameter("ore_to"));
+		    		minuti_to = Integer.parseInt(request.getParameter("minuti_to"));
+		    		if (giorno_to < 1 || giorno_to > 31 || mese_to < 1 || mese_to > 12 || anno_to < 1 || ore_to < 0 || ore_to > 23 || minuti_to < 0 || minuti_to > 59)
+		    		{
+		    			// Error nell'inserimento data from
+		    			response.sendRedirect("/project.jsp?Error=Date until not correct!");
+		    		}
+		    
+		    		// DATI INSERITI CORRETTAMENTE
+		    		
+		    	}
+		    	catch (Exception exc) {
+		    		response.sendRedirect("/project.jsp?Error=Empty fields!");
+		    		}    
+		    }
+		    else
+		    {
+		    	// Error nell'inserimento hashtag
+		    	response.sendRedirect("/project.jsp?Error=No hashtag correct!");
+		    	
+		    }
+		}
+		/*
+		Twitter twitter = new TwitterFactory().getInstance(); 
+        String qrySearch = "#Fifa12"; 
+        Query query = new Query(qrySearch); 
+        query.since("2012-09-10");
+       // query.until("2012-09-13");
+        query.setRpp(1);   // max limit 100
+        QueryResult result;
 		try {
-			statuses = twitter.getHomeTimeline();
-			  System.out.println("Showing friends timeline.");
-			    for (Status status : statuses) {
-			        System.out.println(status.getUser().getName() + ":" +
-			                           status.getText());
-			    }
+			result = twitter.search(query); 
+			 System.out.println("*** hits: " + result.getTweets().size()); 
+		        for (Tweet tweet : result.getTweets()) { 
+		                System.out.println("GeoLocation: " + tweet.getGeoLocation()); 
+		                //System.out.println("               ("+ tweet.getFromUser() +")"+ 
+	//	":" + tweet.getText()); 
+		             
+		      Status status = twitter.showStatus(tweet.getId());          
+		      System.out.println("status: " + status.getPlace()); 
+           
+		    
+		      
+		                 
+		        } // fine for
 		} catch (TwitterException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-	  
+		} 
+	*/
+		//String a = request.getParameter("hashtag");	
+		//response.sendRedirect("/project.jsp?Location=" + a);
+
 		
 		
-		
-		response.setContentType("text/plain");
-		response.getWriter().println("Hello, world");
-	
 }
 }
--- Project/war/project.jsp
+++ Project/war/project.jsp
@@ -16,19 +16,65 @@
 <html>
   <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <link type="text/css" rel="stylesheet" href="/stylesheets/project.css" />
     <title>Progetto</title>
   </head>
 
-  <body>
-    <h1>PROGETTO</h1>
-	
-    <table>
-      <tr>
-        <td colspan="2" style="font-weight:bold;">Available Servlets:</td>        
-      </tr>
-      <tr>
-        <td><a href="project">Project</a></td>
-      </tr>
-    </table>
+  <body class="sfondo">
+  
+  <%
+  		
+    	String Location = request.getParameter("Location");
+    	if (Location == null) {
+        	Location = "default";
+    	}
+    	pageContext.setAttribute("Location", Location);
+	%>
+  
+	<div class="container">
+	<div class="container_2">
+  
+  		<!-- titolo in alto -->
+  		<div class="top">
+        	PROGETTO SISTEMI DISTRIBUITI
+        </div>
+  
+  		<!-- Centro della pagina con mappa a sinistra e inserimento dati a destra -->
+  		<div class="center">
+        
+        	<div class="mappa">
+        		
+        	</div>
+        
+        	<div class="ins_dati">
+            	<!-- Inserimento dati per query su twitter -->
+       			<form action="/project" method="post">
+                <div class="riga_dati">HashTags: <input type="text" size="50" name="hashtag" placeholder="#example,#example2"/></div>
+                <div class="riga_dati"><div>Since Date</div>
+                Day: <input type="text" size="2" name="giorno_from" maxlength="2" placeholder="00"/> Month: <input type="text" size="2" name="mese_from" maxlength="2" placeholder="00"/> Year: <input type="text" size="4" name="anno_from" maxlength="4" placeholder="0000"/> hour: <input type="text" size="2" name="ore_from" maxlength="2" placeholder="hh"/>:<input type="text" size="2" name="minuti_from" maxlength="2" placeholder="mm"/></div>
+                <div class="riga_dati"> <div>Until Date</div>
+                Day: <input type="text" size="2" name="giorno_to" maxlength="2" placeholder="00"/> Month: <input type="text" size="2" name="mese_to" maxlength="2" placeholder="00"/> Year: <input type="text" size="4" name="anno_to" maxlength="4" placeholder="0000"/> hour: <input type="text" size="2" name="ore_to" maxlength="2" placeholder="hh"/>:<input type="text" size="2" name="minuti_to" maxlength="2" placeholder="mm"/> </div>
+                <div class="riga_dati"><input type="submit" value="Ricerca" /></div>
+                </form>
+        	</div>
+        
+        </div>
+  
+    
+       <%
+	  		String Error = request.getParameter("Error");
+			if (Error != null) {
+        		%>
+                	<p style="font-style:italic; color:red; font-size:16px;"> ERROR: <%=Error%></p>
+				<%
+    		}
+  
+	   %>
+    
+    <p>Hello, ${fn:escapeXml(Location)}!</p>
+    
+    </div> 
+    </div>
+    
   </body>
 </html>
