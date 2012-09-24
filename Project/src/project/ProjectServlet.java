cc9891f35da317d256970fe3fbfd79e1f21712ae
Gilli Daniele <daniele.gilli@studenti.unipr.it>
2012-09-24 11:49:56 +0200

    Collegamento interfaccia con ricerche
    Salvataggio su datastore.
    Inseriti controlli su ricerche oltre 350 tweet all'ora.
    Mancano alcuni controlli.

--- Project/src/project/ProjectServlet.java
+++ Project/src/project/ProjectServlet.java
@@ -4,102 +4,351 @@ import java.io.IOException;
 
 
 import javax.servlet.http.*;
-import java.util.List;
 
-import twitter4j.*;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Tweet;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.User;
 
+import com.google.appengine.api.ThreadManager;
+//import com.google.appengine.api.search.query.QueryParser.query_return;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Key;
 //import java.lang.Exception;
 
-@SuppressWarnings({ "serial" , "unused"})
+import java.util.*;
+import java.lang.*;
+
+
+@SuppressWarnings({ "serial" })
 public class ProjectServlet extends HttpServlet {
+	
+	
+	
+	final static int num_tweet_page = 1;
+	private Thread thread;
+	private boolean fermato = false;
+	public boolean ricerca = false;
+	
+	// Per orario
+	Calendar calendar = new GregorianCalendar();
+	
+	DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+	Key key_util = KeyFactory.createKey("Parametri", "util_param");
+	Entity entity_util = new Entity("util",key_util);
+	
+	private boolean continuo_ricerca = false;
+	private long last_id;
+	
+	private boolean lim_superato = false;
+	
+
+	
+	
+	class Task implements Runnable {
+		String hash, date_s, date_u;
+		
+		 Task(String hashtag, String date_since, String date_until) {
+			 hash = hashtag;
+			 date_s = date_since;
+			 date_u = date_until;
+		 }
+
+		  public void run() {
+				boolean finito = false;
+				int count_page = 1;
+				
+				// Modifica Paramatro Ricerca nel DS
+				entity_util.setProperty("Ricerca", "Libera");
+				datastore.put(entity_util);
+				
+				//creo il datastore
+			//	DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+				
+				Twitter twitter = new TwitterFactory().getInstance();
+				//creo la chiave radice
+				//Key HashKey = KeyFactory.createKey("Ricerche", "#progettosd 2012-09-10 2012-09-15");
+				Key HashKey = KeyFactory.createKey("Ricerche", hash + " " + date_s + " " + date_u);
+				
+				// Query su DB twitter
+				Query twquery = new Query(hash);
+				twquery.rpp(num_tweet_page);
+				QueryResult result = null;
+				twquery.since(date_s);
+				twquery.until(date_u);
+			
+					// WHILE RICERCA TOTALE CON TIMER
+					// inizializzazione parametri per ogni hashtags
+					finito=false;  
+					continuo_ricerca = false;
+					lim_superato = false;
+					last_id = 0;
+					
+					while (finito == false)
+					{
+						// Verifica se continua ricerca precedente
+						if (continuo_ricerca == true)
+						{
+						twquery.maxId(last_id-1);
+						}
+						
+						// WHILE DELLE QUERY PAGINATE
+						count_page =1;
+						while (count_page <= 5)
+						{
+							entity_util.setProperty("Ricerca", "Attiva");
+							datastore.put(entity_util);
+		
+							
+							//page 1
+							//System.out.println("PAGINA 11111111111111111111111111111111111111111111111111111111111");
+							twquery.setPage(count_page);
+							try {
+								result = twitter.search(twquery);
+								
+				
+								System.out.println("size " + result.getTweets().size() + "  res page" + result.getResultsPerPage());
+								
+								int count =0;
+								
+								for (Tweet tweet : result.getTweets()) {	count ++;
+								last_id = tweet.getId();
+								Status status = twitter.showStatus(tweet.getId());
+								User xx =  twitter.showUser(tweet.getFromUserId());
+								
+								System.out.println(	"Numero di elementi:" + count);
+								System.out.println(tweet.getGeoLocation() + tweet.getLocation() +"  ID TWEET "+ tweet.getId() +  " ID USER "+ tweet.getFromUserId() + tweet.getFromUser() + "  " +
+										tweet.getText() + tweet.getCreatedAt() + tweet.getAnnotations());
+								
+								
+								//salvo oggetto nel datastore
+								Entity entity = new Entity(hash, HashKey );
+								entity.setProperty("IDtweet", tweet.getId());
+								entity.setProperty("IDuser", tweet.getFromUserId());
+								
+								//System.out.println( "   TW PLACE  " + tweet.getPlace());
+								
+								
+								//GEOLOCATION
+								if ((tweet.getGeoLocation()!=null) && (tweet.getGeoLocation().getLatitude()!= (0.0) && tweet.getGeoLocation().getLongitude()!=(0.0)))
+								{
+									
+									System.out.println( "GEOLOCATION" + tweet.getGeoLocation().getLatitude() +"  "+  tweet.getGeoLocation().getLongitude() );
+									entity.setProperty("latitude", tweet.getGeoLocation().getLatitude());
+									entity.setProperty("longitude", tweet.getGeoLocation().getLongitude());
+									//datastore.put(entity);
+								}
+								
+								//STATUS
+								else if ( status.getPlace() !=null)  
+								{
+										
+										String [] coord = GoogleGeoCode.getLocation(status.getPlace().getFullName());
+										entity.setProperty("latitude", coord[0]);
+										entity.setProperty("longitude", coord[1]);
+										System.out.println( "STATUS  " + status.getPlace() +  "   COOOORD" +  status.getPlace().getGeometryCoordinates() );
+										//System.out.println( "STATUS  " + status.getPlace() +  "   GEOLOC" +  status.getGeoLocation());
+										System.out.println( "STATUS LOCATION" +  "lat:" + coord[0]   + "   long" + coord[1] );
+										//datastore.put(entity);
+										
+									
+								}
+								
+								//USER
+								else if (xx.getLocation() != null &&  !xx.getLocation().isEmpty() ) //user
+								{
+									// Possibile exception con place inventati
+									try
+									{
+										System.out.println( "USER \n LOCATION" + xx.getLocation() );
+										String [] coord = GoogleGeoCode.getLocation(xx.getLocation());
+										entity.setProperty("latitude", coord[0]);
+										entity.setProperty("longitude", coord[1]);
+										System.out.println(	"GEOCODE SERVICE   lat:" + coord[0]   + "   long" + coord[1] );
+										//datastore.put(entity);
+									}
+									catch(Exception e)
+									{
+										System.out.println( "Eccezione: nessuna posizione trovata");
+									}
+								}
+								
+								else
+								{
+									System.out.println( "nessuna posizione trovata");
+									//salvataggio
+									//datastore.put(entity);
+								}
+								
+								datastore.put(entity);
+								}
+							}
+									
+							 catch (TwitterException e) {
+								// TODO Auto-generated catch block
+								 
+								// limite superato 
+								e.printStackTrace();
+								count_page = 6;
+								lim_superato =true;
+							}
+							
+							// Se trovati meno di 70 quindi query finita
+							System.out.println( "Fine pagina: " + count_page );
+							count_page++;
+							if (result.getTweets().size() < num_tweet_page && lim_superato == false)
+							{
+								count_page =6;
+								finito = true;
+							}// FINE IF 
+							else // se non finita la ricerca
+							{
+								if (count_page == 5 || lim_superato == true)
+								{
+									continuo_ricerca = true;
+								}
+							}
+							
+							
+						} // FINE WHILE DELLE QUERY PAGINATE
+						System.out.println("FINE query paginate");
+						
+						if (finito == false)
+						{
+							
+							try {
+								entity_util.setProperty("Ricerca", "Bloccata");
+								datastore.put(entity_util);
+								
+								
+								System.out.println("-------- 350 tweet, prossima ricerca fra un'ora -------");
+								
+								// salva i minuti
+								 int minuti = calendar.get(Calendar.MINUTE);
+								 entity_util.setProperty("Minuti", minuti);
+									datastore.put(entity_util);
+					
+								
+									
+								Thread.sleep(300000); // dopo un'ora
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+						
+						if (fermato == true)
+						{
+							finito = true;
+						}
+						
+					}// FINE WHILE RICERCA TOTALE CON TIMER
+					System.out.println("FINE totale");
+				
+				
+				entity_util.setProperty("Ricerca", "Libera");
+				datastore.put(entity_util);
+				fermato = false;
+				finito = false;
+				
+				
+		  }
+
+	}
+	
 	
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws IOException {
 	
-		// Prende parametri inseriti
-		String [] hashtags = request.getParameter("hashtag").split(",");
-		int giorno_from, mese_from, anno_from, ore_from, minuti_from;
-		int giorno_to, mese_to, anno_to, ore_to, minuti_to;
+	
 		
+			
+		//------------------------------------------------------------------
+		// FINE SOTTO-THREAD
+		//------------------------------------------------------------------
 		
-		
-		
-		for(String hash:hashtags) // per ogni hashtag
+		if (request.getParameter("ferma") != null)
 		{
-		    if (hash.startsWith("#"))  // se inizia con '#' OK
-		    {
-		    	try {
-		    		// Controllo sul data from
-		    		giorno_from = Integer.parseInt(request.getParameter("giorno_from"));
-		    		mese_from = Integer.parseInt(request.getParameter("mese_from"));
-		    		anno_from = Integer.parseInt(request.getParameter("anno_from"));
-		    		ore_from = Integer.parseInt(request.getParameter("ore_from"));
-		    		minuti_from = Integer.parseInt(request.getParameter("minuti_from"));
-		    		if (giorno_from < 1 || giorno_from > 31 || mese_from < 1 || mese_from > 12 || anno_from < 1 || ore_from < 0 || ore_from > 23 || minuti_from < 0 || minuti_from > 59 )
-		    		{
-		    			// Error nell'inserimento data from
-		    			response.sendRedirect("/project.jsp?Error=Date from not correct!");
-		    		}
-		    	
-		    		// Controllo sul data until
-		    		giorno_to = Integer.parseInt(request.getParameter("giorno_to"));
-		    		mese_to = Integer.parseInt(request.getParameter("mese_to"));
-		    		anno_to = Integer.parseInt(request.getParameter("anno_to"));
-		    		ore_to = Integer.parseInt(request.getParameter("ore_to"));
-		    		minuti_to = Integer.parseInt(request.getParameter("minuti_to"));
-		    		if (giorno_to < 1 || giorno_to > 31 || mese_to < 1 || mese_to > 12 || anno_to < 1 || ore_to < 0 || ore_to > 23 || minuti_to < 0 || minuti_to > 59)
-		    		{
-		    			// Error nell'inserimento data from
-		    			response.sendRedirect("/project.jsp?Error=Date until not correct!");
-		    		}
-		    
-		    		// DATI INSERITI CORRETTAMENTE
-		    		
-		    	}
-		    	catch (Exception exc) {
-		    		response.sendRedirect("/project.jsp?Error=Empty fields!");
-		    		}    
-		    }
-		    else
-		    {
-		    	// Error nell'inserimento hashtag
-		    	response.sendRedirect("/project.jsp?Error=No hashtag correct!");
-		    	
-		    }
+			fermato = true;
+			System.out.println("INTERROTTOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+			response.sendRedirect("/project.jsp?Ricerca=incorso");
+			
 		}
-		/*
-		Twitter twitter = new TwitterFactory().getInstance(); 
-        String qrySearch = "#Fifa12"; 
-        Query query = new Query(qrySearch); 
-        query.since("2012-09-10");
-       // query.until("2012-09-13");
-        query.setRpp(1);   // max limit 100
-        QueryResult result;
-		try {
-			result = twitter.search(query); 
-			 System.out.println("*** hits: " + result.getTweets().size()); 
-		        for (Tweet tweet : result.getTweets()) { 
-		                System.out.println("GeoLocation: " + tweet.getGeoLocation()); 
-		                //System.out.println("               ("+ tweet.getFromUser() +")"+ 
-	//	":" + tweet.getText()); 
-		             
-		      Status status = twitter.showStatus(tweet.getId());          
-		      System.out.println("status: " + status.getPlace()); 
-           
-		    
-		      
-		                 
-		        } // fine for
-		} catch (TwitterException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-	*/
-		//String a = request.getParameter("hashtag");	
-		//response.sendRedirect("/project.jsp?Location=" + a);
+		else
+		{	
+			// Controlli hashtag
+			if (request.getParameter("hashtag") == "") // se vuoto
+			{
+				response.sendRedirect("/project.jsp?Error=Empty hashtag field!");
+			}
+			
+			String hashtag = request.getParameter("hashtag"); // divido gli hashtag
+			if (!hashtag.startsWith("#"))  // se non corretto l'hashtag
+			{
+				response.sendRedirect("/project.jsp?Error=No hashtag correct!");
+			}
+	
+			
+			int giorno_from, mese_from, anno_from;      //, ore_from, minuti_from;
+			int giorno_to, mese_to, anno_to;    //, ore_to, minuti_to;
+			
+			try {
+	    		// Controllo sul data from
+	    		giorno_from = Integer.parseInt(request.getParameter("giorno_from"));
+	    		mese_from = Integer.parseInt(request.getParameter("mese_from"));
+	    		anno_from = Integer.parseInt(request.getParameter("anno_from"));
+	    		//ore_from = Integer.parseInt(request.getParameter("ore_from"));
+	    		//minuti_from = Integer.parseInt(request.getParameter("minuti_from"));
+	    		if (giorno_from < 1 || giorno_from > 31 || mese_from < 1 || mese_from > 12 || anno_from < 1)
+	    		{
+	    			// Error nell'inserimento data from
+	    			response.sendRedirect("/project.jsp?Error=Date from not correct!");
+	    		}
+	    	
+	    		// Controllo sul data until
+	    		giorno_to = Integer.parseInt(request.getParameter("giorno_to"));
+	    		mese_to = Integer.parseInt(request.getParameter("mese_to"));
+	    		anno_to = Integer.parseInt(request.getParameter("anno_to"));
+	    		//ore_to = Integer.parseInt(request.getParameter("ore_to"));
+	    		//minuti_to = Integer.parseInt(request.getParameter("minuti_to"));
+	    		if (giorno_to < 1 || giorno_to > 31 || mese_to < 1 || mese_to > 12 || anno_to < 1)
+	    		{
+	    			// Error nell'inserimento data from
+	    			response.sendRedirect("/project.jsp?Error=Date until not correct!");
+	    		}
+	    
+	    		// DATI INSERITI CORRETTAMENTE
+	    		// hash  la hashtag
+	    		String date_since = anno_from + "-" + mese_from + "-" + giorno_from;
+	    		String date_until = anno_to + "-" + mese_to + "-" + giorno_to;
+				 
+	    	
+	    		thread = ThreadManager.createBackgroundThread(new Task(hashtag,date_since,date_until));
+	    		thread.start();
+	    		
+	    		//s.stop();
+	    		
+	    		//s.service(request, response);
+	    	}
+	    	catch (Exception exc) {
+	    		response.sendRedirect("/project.jsp?Error=Empty fields!");
+	    		}    
+			
+			response.sendRedirect("/project.jsp?Ricerca=Attivata");
+		}
+		
+		//s = new SearchServlet();
+		//s.init();
+	
+	}
+	
+}
 
-		
-		
-}
-}
--- Project/war/project.jsp
+++ Project/war/project.jsp
@@ -1,5 +1,8 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ page import="java.util.List" %>
+<%@ page import="java.util.*" %>
+<%@ page import="java.lang.*" %>
+<%@ page import="java.lang.Integer" %>
 <%@ page import="com.google.appengine.api.users.User" %>
 <%@ page import="com.google.appengine.api.users.UserService" %>
 <%@ page import="com.google.appengine.api.users.UserServiceFactory" %>
@@ -10,26 +13,142 @@
 <%@ page import="com.google.appengine.api.datastore.FetchOptions" %>
 <%@ page import="com.google.appengine.api.datastore.Key" %>
 <%@ page import="com.google.appengine.api.datastore.KeyFactory" %>
+<%@ page import="com.google.appengine.api.ThreadManager" %>
+
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
 
 
+<%!
+	String Ricerca = "Libera";
+	
+  %>
+
+
+<%
+
+
+/*
+		class Task implements Runnable {
+    
+		String str;
+        Task(String s) { str = s; }
+        public void run() {
+             {
+					  try 
+					  {
+						  DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+						  String Ricerca_thread = "Libera";
+						  Query dataquery  = new Query ("util");
+							List<Entity> resultdata = datastore.prepare(dataquery).asList(FetchOptions.Builder.withDefaults());
+						  	
+						  	while (Ricerca_thread != "Libera") 
+						  	{
+								
+								
+								System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+							  	Thread.sleep(5000);
+								
+								if (resultdata.isEmpty())
+								{
+									System.out.println(	"LISTA VUOTA");
+								}
+								else
+								{	
+									for (Entity post : resultdata) 
+									{
+										Ricerca_thread = post.getProperty("Ricerca").toString();  // prende il parametro ricerca
+										//System.out.println(	"ID TWEET:" + post.getProperty("IDtweet") + "  ID USER:"+ post.getProperty("IDuser") +
+										//	"  COORD:" + post.getProperty("latitude") +"  "+  post.getProperty("longitude") + "KEY" + post.getKey() );
+									}			
+								}
+							  
+						  }
+					  } 
+					  catch (InterruptedException ex) 
+					  {
+						  throw new RuntimeException("Interrupted in loop:", ex);
+					  } 
+				  }
+        }
+    }
+
+*/
+%>
+
 <html>
   <head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  
+   <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  
+  <%
+  
+  String Avvio_Ricerca = request.getParameter("Ricerca");
+	
+	// Controllo sulla ricerca
+	
+	if (Avvio_Ricerca != null)
+	{
+		if (Avvio_Ricerca.compareTo("Attivata") == 0)
+		{
+		
+			Ricerca = "Attiva";  // Indica che è stata attivata una ricerca
+			%>
+			<meta http-equiv="refresh" content="10; URL='/project.jsp?Ricerca=incorso'">
+			<%
+    	}
+		
+		else if (Avvio_Ricerca.compareTo("incorso") == 0)
+		{
+		  	DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+			Key key_util = KeyFactory.createKey("Parametri", "util_param");
+			// Vedere se vi è una ricerca attiva
+			Query dataquery  = new Query ("util",key_util).setAncestor(key_util);
+			List<Entity> resultdata = datastore.prepare(dataquery).asList(FetchOptions.Builder.withDefaults());
+			
+			if (resultdata.isEmpty())
+			{
+				System.out.println(	"LISTA VUOTA");
+			}
+			else
+			{	
+				for (Entity post : resultdata) 
+				{
+					Ricerca = post.getProperty("Ricerca").toString();  // prende il parametro ricerca
+					System.out.println(Ricerca);
+					//System.out.println(	"ID TWEET:" + post.getProperty("IDtweet") + "  ID USER:"+ post.getProperty("IDuser") +
+					//	"  COORD:" + post.getProperty("latitude") +"  "+  post.getProperty("longitude") + "KEY" + post.getKey() );
+				}			
+			}
+			if (Ricerca.compareTo("Libera") != 0)
+			{
+				%>
+					<meta http-equiv="refresh" content="10; URL='/project.jsp?Ricerca=incorso'">
+				<%
+    		}
+			else
+			{
+				%>
+					<meta http-equiv="refresh" content="1; URL='/project.jsp'">
+				<%
+    		}
+		
+		}
+		
+	}
+	
+	
+  	
+	%>
+   
     <link type="text/css" rel="stylesheet" href="/stylesheets/project.css" />
     <title>Progetto</title>
+    
+    
+    
   </head>
 
   <body class="sfondo">
   
-  <%
-  		
-    	String Location = request.getParameter("Location");
-    	if (Location == null) {
-        	Location = "default";
-    	}
-    	pageContext.setAttribute("Location", Location);
-	%>
   
 	<div class="container">
 	<div class="container_2">
@@ -49,29 +168,92 @@
         	<div class="ins_dati">
             	<!-- Inserimento dati per query su twitter -->
        			<form action="/project" method="post">
-                <div class="riga_dati">HashTags: <input type="text" size="50" name="hashtag" placeholder="#example,#example2"/></div>
+                <div class="riga_dati">HashTag: <input type="text" size="50" name="hashtag" placeholder="#example"/></div>
                 <div class="riga_dati"><div>Since Date</div>
-                Day: <input type="text" size="2" name="giorno_from" maxlength="2" placeholder="00"/> Month: <input type="text" size="2" name="mese_from" maxlength="2" placeholder="00"/> Year: <input type="text" size="4" name="anno_from" maxlength="4" placeholder="0000"/> hour: <input type="text" size="2" name="ore_from" maxlength="2" placeholder="hh"/>:<input type="text" size="2" name="minuti_from" maxlength="2" placeholder="mm"/></div>
+                Day: <input type="text" size="2" name="giorno_from" maxlength="2" placeholder="00"/> Month: <input type="text" size="2" name="mese_from" maxlength="2" placeholder="00"/> Year: <input type="text" size="4" name="anno_from" maxlength="4" placeholder="0000"/>  </div>
+                <!-- hour: <input type="text" size="2" name="ore_from" maxlength="2" placeholder="hh"/>:<input type="text" size="2" name="minuti_from" maxlength="2" placeholder="mm"/></div> -->
                 <div class="riga_dati"> <div>Until Date</div>
-                Day: <input type="text" size="2" name="giorno_to" maxlength="2" placeholder="00"/> Month: <input type="text" size="2" name="mese_to" maxlength="2" placeholder="00"/> Year: <input type="text" size="4" name="anno_to" maxlength="4" placeholder="0000"/> hour: <input type="text" size="2" name="ore_to" maxlength="2" placeholder="hh"/>:<input type="text" size="2" name="minuti_to" maxlength="2" placeholder="mm"/> </div>
-                <div class="riga_dati"><input type="submit" value="Ricerca" /></div>
+                Day: <input type="text" size="2" name="giorno_to" maxlength="2" placeholder="00"/> Month: <input type="text" size="2" name="mese_to" maxlength="2" placeholder="00"/> Year: <input type="text" size="4" name="anno_to" maxlength="4" placeholder="0000"/> </div>
+                <!-- hour: <input type="text" size="2" name="ore_to" maxlength="2" placeholder="hh"/>:<input type="text" size="2" name="minuti_to" maxlength="2" placeholder="mm"/> </div> -->
+                <div class="riga_dati"><input type="submit" value="Ricerca" <% if (Ricerca.compareTo("Libera") != 0 ) { %> disabled <% }  %> /></div>
                 </form>
+                
+                
+                
+                <% // RIQUADRO RICERCA
+					if (Ricerca.compareTo("Libera") != 0 )
+					{
+					%>
+       				<div class="ricerca_attiva">
+                	<p>RICERCA IN CORSO</p>
+            		
+                    
+                    <%  if  (Ricerca.compareTo("Bloccata") == 0) 
+						{
+							DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+							Key key_util = KeyFactory.createKey("Parametri", "util_param");
+							 Query dataquery  = new Query ("util",key_util).setAncestor(key_util);
+							List<Entity> resultdata = datastore.prepare(dataquery).asList(FetchOptions.Builder.withDefaults());
+							
+							 Calendar calendar = new GregorianCalendar();
+			
+							int dif_minuti =0;  
+							
+							if (resultdata.isEmpty())
+							{
+								System.out.println(	"LISTA VUOTA");
+							}
+							else
+							{	
+								for (Entity post : resultdata) 
+								{
+									
+									int minuti_att = calendar.get(Calendar.MINUTE);
+									int minuti_ric = Integer.parseInt(post.getProperty("Minuti").toString());
+									dif_minuti = (60 - minuti_att +  minuti_ric);  // prende il parametro ricerca
+									//System.out.println(dif_minuti);
+										//System.out.println(	"ID TWEET:" + post.getProperty("IDtweet") + "  ID USER:"+ post.getProperty("IDuser") +
+										//	"  COORD:" + post.getProperty("latitude") +"  "+  post.getProperty("longitude") + "KEY" + post.getKey() );
+										
+								}	
+								
+								%>  <div class="riga_dati"> Mancano <%=dif_minuti%> minuti alla prossima ricerca </div>  <%		
+							}
+						}  
+					%>
+                    
+      				</div>
+        		
+                <form action="/project" method="post">
+                <div class="riga_dati"><input type="submit" value="Stop" /></div>
+                 <input type="hidden" name="ferma" value="1"/>   <!-- 1 ferma -->
+                </form>
+                
+                	<%
+					}
+				%>
+                
         	</div>
         
         </div>
   
     
        <%
+	   
+	   		// CONTROLLO SE AVVENUTO ERRORE NELL'INSERIMENTO
 	  		String Error = request.getParameter("Error");
 			if (Error != null) {
         		%>
                 	<p style="font-style:italic; color:red; font-size:16px;"> ERROR: <%=Error%></p>
 				<%
-    		}
+    		}  
   
-	   %>
-    
-    <p>Hello, ${fn:escapeXml(Location)}!</p>
+   			
+				%>
+        
+        
+   
+    <!-- <p>Hello, ${fn:escapeXml(ricerca)}!</p> -->
     
     </div> 
     </div>
